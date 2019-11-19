b80290c9f52ad92c6013aef00576b089adc1a66c
chendan5 <chendan5@lenovo.com>
2019-11-19 19:06:59 +0800

    main test code

--- src/com/ann/Main.java
+++ src/com/ann/Main.java
@@ -5,15 +5,17 @@ import com.ann.leetCode.medium.LeetCode32LongestValidParenthese;
 import com.ann.leetCode.medium.LeetCode33Search;
 import com.ann.leetCode.medium.LeetCodeClimbStairsDiffStep;
 import com.ann.leetCode.medium.LeetCode_64_MinPathSum;
+import com.ann.leetCode.simple.LeetCode121MaxProfit;
+import com.ann.leetCode.simple.LeetCode122MaxProfit2;
 //import com.ann.leetCode.simple.LeetCode860LemonadeChange;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[][] nums = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        int[] nums = {1,2,3,4,5};
         String s = "()(()()";
-        LeetCode32LongestValidParenthese model = new LeetCode32LongestValidParenthese();
-        System.out.println(model.longestValidParentheses1(s));
+        LeetCode122MaxProfit2 model = new LeetCode122MaxProfit2();
+        System.out.println(model.maxProfit(nums));
     }
 }

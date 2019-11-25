5a1ade1d22bd6a85af72b48b1ce583b780f0ddad
chendan5 <chendan5@lenovo.com>
2019-11-25 12:07:10 +0800

    leetcode 198

--- src/com/ann/leetCode/simple/LeetCode198Rob.java
+++ src/com/ann/leetCode/simple/LeetCode198Rob.java
@@ -1,8 +1,39 @@
 package com.ann.leetCode.simple;
 
 public class LeetCode198Rob {
+    /**
+     * #1
+     * dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+     * @param nums
+     * @return
+     */
     public int rob(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int[] robNum = new int[nums.length];
+        robNum[0] = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            robNum[i] = Math.max(robNum[i - 1], i < 2 ? nums[i]: nums[i] + robNum[i - 2]);
+        }
+        return robNum[nums.length - 1];
+    }
 
-        return 0;
+    /**
+     * #2 improvement of #1
+     * @param nums
+     * @return
+     */
+    public int rob1(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int robMaxPrev1 = nums[0],
+            robMaxPrev2 = 0,
+            robMax;
+        for (int i = 1; i < nums.length; ++i) {
+            robMax = Math.max(robMaxPrev1, robMaxPrev2 + nums[i]);
+            robMaxPrev2 = robMaxPrev1;
+            robMaxPrev1 = robMax;
+        }
+        return robMaxPrev1;
     }
 }

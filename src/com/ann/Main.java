d043c307e1f3706a0ab096238318c1b55fc3e128
chendan5 <chendan5@lenovo.com>
2019-11-25 15:39:38 +0800

    leetcode

--- src/com/ann/Main.java
+++ src/com/ann/Main.java
@@ -4,6 +4,7 @@ package com.ann;
 import com.ann.leetCode.medium.*;
 import com.ann.leetCode.simple.LeetCode121MaxProfit;
 import com.ann.leetCode.simple.LeetCode122MaxProfit2;
+import com.ann.leetCode.simple.LeetCode198Rob;
 
 import java.util.List;
 //import com.ann.leetCode.simple.LeetCode860LemonadeChange;
@@ -12,10 +13,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-     /*   int[] nums = { 7, 4, 5, 6, 9, 10, 4, 1, 6, 5, 9};
         String s = "()(()()";
-        LeetCode122MaxProfit2 model = new LeetCode122MaxProfit2();
-        System.out.println(model.maxProfit2(nums));*/
+        int[] nums = { 1};
+        LeetCode213Rob2 model = new LeetCode213Rob2();
+        System.out.println(model.rob(nums));
        /* LeetCode208Trie trie = new LeetCode208Trie();
         trie.insert("apple");
         System.out.println(trie.search("apple"));
@@ -23,11 +24,11 @@ public class Main {
         System.out.println(trie.startsWith("app"));
         trie.insert("app");
         System.out.println(trie.search("app"));*/
-       char[][] board = {{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}};
+       /*char[][] board = {{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}};
        String[] words = {"oath", "pea", "eat", "rain"};
        LeetCode212FindWords model = new LeetCode212FindWords();
         List<String> wordList = model.findWords(board, words);
        for (String str: wordList)
-           System.out.println(str);
+           System.out.println(str);*/
     }
 }

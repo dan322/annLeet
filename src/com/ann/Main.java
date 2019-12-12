575bdc309c64725fd7d84d7a9460ed65c8c726f5
chendan5 <chendan5@lenovo.com>
2019-12-12 18:26:23 +0800

    leet code test

--- src/com/ann/Main.java
+++ src/com/ann/Main.java
@@ -167,8 +167,15 @@ public class Main {
 //        LeetCode493ReversePairs model = new LeetCode493ReversePairs();
 //        int[] nums = {1, 3, 2, 3, 1};
 //        System.out.println(model.reversePairs(nums));
-        LeetCode557ReverseWord model = new LeetCode557ReverseWord();
+        LeetCode221MaximalSquare model = new LeetCode221MaximalSquare();
         String str = " Let's take LeetCode contest ";
-        System.out.println(model.reverseWords3(str));
+        char[][] matrix = {
+                {'1', '1', '1', '1', '1', '1', '1', '1'},
+                {'1', '1', '1', '1', '1', '1', '1', '0'},
+                {'1', '1', '1', '1', '1', '1', '1', '0'},
+                {'1', '1', '1', '1', '1', '0', '0', '0'},
+                {'0', '1', '1', '1', '1', '0', '0', '0'},
+        };
+        System.out.println(model.maximalSquare2(matrix));
     }
 }

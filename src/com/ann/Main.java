54cb52cb74c2481e9997a39f1e8c29a28cf4df50
chendan5 <chendan5@lenovo.com>
2019-12-27 18:50:50 +0800

    leetcode 300 lengthOfLIS

--- src/com/ann/Main.java
+++ src/com/ann/Main.java
@@ -167,8 +167,8 @@ public class Main {
 //        LeetCode493ReversePairs model = new LeetCode493ReversePairs();
 //        int[] nums = {1, 3, 2, 3, 1};
 //        System.out.println(model.reversePairs(nums));
-        LeetCode151ReverseWords model = new LeetCode151ReverseWords();
-        String str = "the sky is  blue!   ";
+        LeetCode300LengthOfLIS model = new LeetCode300LengthOfLIS();
+        String str = " hello  world!  ";
         char[][] matrix = {
                 {'1', '1', '1', '1', '1', '1', '1', '1'},
                 {'1', '1', '1', '1', '1', '1', '1', '0'},
@@ -176,6 +176,7 @@ public class Main {
                 {'1', '1', '1', '1', '1', '0', '0', '0'},
                 {'0', '1', '1', '1', '1', '0', '0', '0'},
         };
-        System.out.println(model.reverseWords(str));
+        int[] nums = {10,9,2,5,3,7,101,18};
+        System.out.println(model.lengthOfLIS(nums));
     }
 }
--- src/com/ann/leetCode/medium/LeetCode300LengthOfLIS.java
+++ src/com/ann/leetCode/medium/LeetCode300LengthOfLIS.java
@@ -3,14 +3,41 @@ package com.ann.leetCode.medium;
 import java.util.Arrays;
 
 public class LeetCode300LengthOfLIS {
+    int[] increaseNum;
 
     /**
-     * todo ann
      * [10,9,2,5,3,7,101,18]
      * @param nums
      * @return
      */
     public int lengthOfLIS(int[] nums) {
-        return 0;
+        increaseNum = new int[nums.length];
+        int maxLength = 0;
+        for (int num: nums) {
+            maxLength = Math.max(maxLength, getNumPosition(num, maxLength));
+        }
+        return maxLength;
+    }
+
+    public int getNumPosition(int num, int len)
+    {
+        if (len == 0) {
+            increaseNum[len] = num;
+            return len + 1;
+        }
+        int left = 0, right = len - 1, middle;
+        while (left < right) {
+            middle = left +  ((right - left) >> 1);
+            if (increaseNum[middle] < num) {
+                left = middle + 1;
+            } else if (increaseNum[middle] == num) {
+                return middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        int index = increaseNum[left] > num ? left: left + 1;
+        increaseNum[index] = num;
+        return index + 1;
     }
 }

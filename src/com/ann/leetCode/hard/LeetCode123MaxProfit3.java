ac744044fb5692ae27f6dd872f36cbe9b1e8dd94
chendan5 <chendan5@lenovo.com>
2019-12-30 19:19:54 +0800

    leetcode 123

--- src/com/ann/leetCode/hard/LeetCode123MaxProfit3.java
+++ src/com/ann/leetCode/hard/LeetCode123MaxProfit3.java
@@ -1,7 +1,24 @@
 package com.ann.leetCode.hard;
 
 public class LeetCode123MaxProfit3 {
+    /**
+     * todo ann
+     * 输入: [3,3,5,0,0,3,1,4]
+     * 输出: 6
+     * 输入: [1,2,3,4,5]
+     * 输出: 4
+     * 输入: [7,6,4,3,1]
+     * 输出: 0
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
+        int[][] maxProfits = new int[prices.length][2];
+        int maxProfit;
+        for (int i = 1; i < prices.length; ++i) {
+            maxProfits[i][0] = maxProfits[i - 1][0] + Math.max(0, prices[i] - prices[i - 1]);
+
+        }
         return 0;
     }
 }

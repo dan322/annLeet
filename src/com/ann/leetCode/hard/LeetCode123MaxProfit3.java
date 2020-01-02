d1d469e104f6106db0c0dd173fe42a358c091cc3
chendan5 <chendan5@lenovo.com>
2020-01-02 16:33:02 +0800

    leetcode 123

--- src/com/ann/leetCode/hard/LeetCode123MaxProfit3.java
+++ src/com/ann/leetCode/hard/LeetCode123MaxProfit3.java
@@ -2,7 +2,6 @@ package com.ann.leetCode.hard;
 
 public class LeetCode123MaxProfit3 {
     /**
-     * todo ann
      * 输入: [3,3,5,0,0,3,1,4]
      * 输出: 6
      * 输入: [1,2,3,4,5]
@@ -13,12 +12,35 @@ public class LeetCode123MaxProfit3 {
      * @return
      */
     public int maxProfit(int[] prices) {
-        int[][] maxProfits = new int[prices.length][2];
-        int maxProfit;
-        for (int i = 1; i < prices.length; ++i) {
-            maxProfits[i][0] = maxProfits[i - 1][0] + Math.max(0, prices[i] - prices[i - 1]);
-
+        int[][] maxProfits = new int[prices.length][3];
+        int minPrice;
+        for (int k = 1; k < 3; ++k) {
+            minPrice = prices[0];
+            for (int i = 1; i < prices.length; ++i) {
+                minPrice = Math.min(minPrice, prices[i] - maxProfits[i - 1][k - 1]);
+                maxProfits[i][k] = Math.max(maxProfits[i - 1][k], prices[i] - minPrice);
+            }
         }
+        return maxProfits[prices.length - 1][2];
+    }
+
+    /**
+     * todo ann less space spend
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int[] prices) {
+
+        return 0;
+    }
+
+    /**
+     * todo ann use dp write again
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int prices) {
+
         return 0;
     }
 }

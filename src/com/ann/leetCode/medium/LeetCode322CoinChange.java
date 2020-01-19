5296a2e8e01842655ec424249d118220c0e45565
chendan5 <chendan5@lenovo.com>
2020-01-19 15:12:06 +0800

    leetcode322

--- src/com/ann/leetCode/medium/LeetCode322CoinChange.java
+++ src/com/ann/leetCode/medium/LeetCode322CoinChange.java
@@ -27,29 +27,74 @@ public class LeetCode322CoinChange {
         return -1;
     }
 
+    /**
+     *  F(S) = F(S - C) + 1   F(S)：S 金额所需的最小硬币数量
+     *  S：使用的硬币面值和
+     * @param coins
+     * @param amount
+     * @return
+     */
     public int coinChange1(int[] coins, int amount) {
         Arrays.sort(coins);
         int[] memoryChange = new int[amount];
-        return change(amount, coins, 0, memoryChange);
+        return change(amount, coins, memoryChange);
     }
 
-    public int change(int amount, int[] coins, int changeNum, int[] memoryChange)
+    public int change(int amount, int[] coins, int[] memoryChange)
     {
         if (amount == 0)
-            return changeNum + 1;
+            return 0;
         else if (amount < 0)
             return -1;
+        if (memoryChange[amount - 1] != 0)
+            return memoryChange[amount - 1];
         int minChange = Integer.MAX_VALUE, num;
         for (int i = coins.length - 1; i >= 0; --i) {
-            int amountCur = amount - coins[i];
-            num = memoryChange[amountCur - 1];
-            if (num == 0) {
-                num = change(amountCur, coins, changeNum + 1, memoryChange);
-                memoryChange[amountCur - 1] = num;
-            }
+            num = change(amount - coins[i], coins, memoryChange);
             if (num > -1)
-                minChange = Math.min(minChange, num);
+                minChange = Math.min(minChange, num + 1);
         }
-        return minChange == Integer.MAX_VALUE ? -1: minChange;
+        memoryChange[amount - 1] = minChange == Integer.MAX_VALUE ? -1: minChange;
+        return memoryChange[amount - 1];
+    }
+
+    /**
+     * F(i) 应等于 minj =F(i -cj) + 1   j: j=0…n−1
+     * F(i−cj)+1，cj代表的是每个硬币的面值，所以 F(i)F(i) 是前面能组成的金额的最小值 +1。
+     * bottom-up
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange2(int[] coins, int amount) {
+        int[] memoryChange = new int[amount + 1];
+        Arrays.fill(memoryChange, amount + 1);
+        memoryChange[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.length; ++j) {
+                if (i >= coins[j] && memoryChange[i] > memoryChange[i - coins[j]])
+                    memoryChange[i] = memoryChange[i - coins[j]] + 1;
+            }
+        }
+        return memoryChange[amount] == amount + 1 ? -1: memoryChange[amount];
+    }
+
+    /**
+     * improvement of coinchange2
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange3(int[] coins, int amount) {
+        int[] memoryChange = new int[amount + 1];
+        memoryChange[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            memoryChange[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; ++j) {
+                if (i >= coins[j] && memoryChange[i] > memoryChange[i - coins[j]])
+                    memoryChange[i] = memoryChange[i - coins[j]] + 1;
+            }
+        }
+        return memoryChange[amount] ==  Integer.MAX_VALUE ? -1: memoryChange[amount];
     }
 }

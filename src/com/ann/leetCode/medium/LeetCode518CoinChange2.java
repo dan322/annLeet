0026dc95b00fd1512e495ae8f791e8b24da177f4
chendan5 <chendan5@lenovo.com>
2020-01-20 10:57:34 +0800

    leetcode518

--- src/com/ann/leetCode/medium/LeetCode518CoinChange2.java
+++ src/com/ann/leetCode/medium/LeetCode518CoinChange2.java
@@ -2,8 +2,43 @@ package com.ann.leetCode.medium;
 
 public class LeetCode518CoinChange2 {
 
+    /**
+     * 以基本情况没有硬币开始组合数量。dp[0] = 1，然后其余等于 0。
+     * 遍历所有硬币面值：
+     * 对于每个硬币，我们将从金额 0 遍历到 amount：
+     * 对于每个 x，计算组合数：dp[x] += dp[x - coin]
+     *
+     * @param amount
+     * @param coins
+     * @return
+     */
     public int change(int amount, int[] coins) {
-        return 0;
+        int[] dpChange = new int[amount + 1];
+        dpChange[0] = 1;
+        for (int i = 0; i < coins.length; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                if (coins[i] <= j)
+                    dpChange[j] += dpChange[j - coins[i]];
+            }
+        }
+        return dpChange[amount];
+    }
+
+    /**
+     * improvement of change1
+     * @param amount
+     * @param coins
+     * @return
+     */
+    public int change2(int amount, int[] coins) {
+        int[] dpChange = new int[amount + 1];
+        dpChange[0] = 1;
+        for (int i = 0; i < coins.length; ++i) {
+            for (int j = coins[i]; j <= amount; ++j) {
+                dpChange[j] += dpChange[j - coins[i]];
+            }
+        }
+        return dpChange[amount];
     }
 
     /**

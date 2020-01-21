430fc9099815d9dd7e190647d852c1c013f0ad5b
chendan5 <chendan5@lenovo.com>
2020-01-21 19:01:02 +0800

    leetcode552

--- src/com/ann/leetCode/hard/LeetCode552CheckRecord2.java
+++ src/com/ann/leetCode/hard/LeetCode552CheckRecord2.java
@@ -62,4 +62,37 @@ public class LeetCode552CheckRecord2 {
                 + checkResult[4] + checkResult[5] + checkResult[6];
         return result % 1000000007;
     }
+
+    /**
+     * todo use dp
+     * dp[i] : length of i the accept number total
+     * @param n
+     * @return
+     */
+    public int checkRecord2(int n) {
+        if (n < 1)
+            return 0;
+        int[] checkResult = {1, 0, 0, 0, 1, 1, 0};
+        int[] checkResultCur;
+        for (int i = 2; i <= n; ++i) {
+            checkResultCur = new int[7];
+            checkResultCur[0] = checkResult[4] + checkResult[5] + checkResult[6];
+            checkResultCur[1] = checkResult[0] + checkResult[1] + checkResult[2] + checkResult[3];
+            checkResultCur[2] = checkResult[0] + checkResult[1];
+            checkResultCur[3] = checkResult[2];
+            checkResultCur[4] = checkResult[4] + checkResult[5] + checkResult[6];
+            checkResultCur[5] = checkResult[4];
+            checkResultCur[6] = checkResult[5];
+            checkResult[0] = checkResultCur[0];
+            checkResult[1] = checkResultCur[1];
+            checkResult[2] = checkResultCur[2];
+            checkResult[3] = checkResultCur[3];
+            checkResult[4] = checkResultCur[4];
+            checkResult[5] = checkResultCur[5];
+            checkResult[6] = checkResultCur[6];
+        }
+        int result = checkResult[0] + checkResult[1] + checkResult[2] + checkResult[3]
+                + checkResult[4] + checkResult[5] + checkResult[6];
+        return result % 1000000007;
+    }
 }

c4b7df3809f96ad73a5d2495601962dc1b92de61
chendan5 <chendan5@lenovo.com>
2020-01-22 11:38:57 +0800

    leetcode552

--- src/com/ann/leetCode/hard/LeetCode552CheckRecord2.java
+++ src/com/ann/leetCode/hard/LeetCode552CheckRecord2.java
@@ -31,7 +31,6 @@ public class LeetCode552CheckRecord2 {
     }
 
     /**
-     * todo ann less space
      * no matter with p and localtion of p
      * @param n
      * @return
@@ -39,28 +38,18 @@ public class LeetCode552CheckRecord2 {
     public int checkRecord1(int n) {
         if (n < 1)
             return 0;
-        int[] checkResult = {1, 0, 0, 0, 1, 1, 0};
-        int[] checkResultCur;
+        long a0l0 = 1, a0l1 = 1, a0l2 = 0, a1l0 = 1, a1l1 = 0, a1l2 = 0;
         for (int i = 2; i <= n; ++i) {
-            checkResultCur = new int[7];
-            checkResultCur[0] = checkResult[4] + checkResult[5] + checkResult[6];
-            checkResultCur[1] = checkResult[0] + checkResult[1] + checkResult[2] + checkResult[3];
-            checkResultCur[2] = checkResult[0] + checkResult[1];
-            checkResultCur[3] = checkResult[2];
-            checkResultCur[4] = checkResult[4] + checkResult[5] + checkResult[6];
-            checkResultCur[5] = checkResult[4];
-            checkResultCur[6] = checkResult[5];
-            checkResult[0] = checkResultCur[0];
-            checkResult[1] = checkResultCur[1];
-            checkResult[2] = checkResultCur[2];
-            checkResult[3] = checkResultCur[3];
-            checkResult[4] = checkResultCur[4];
-            checkResult[5] = checkResultCur[5];
-            checkResult[6] = checkResultCur[6];
+            long newA0l0 = (a0l0 + a0l1 + a0l2) % 1000000007;
+            a0l2 = a0l1;
+            a0l1 = a0l0;
+            a0l0 = newA0l0;
+            long newA1l0 = (a0l0 + a1l0 + a1l1 + a1l2) % 1000000007;
+            a1l2 = a1l1;
+            a1l1 = a1l0;
+            a1l0 = newA1l0;
         }
-        int result = checkResult[0] + checkResult[1] + checkResult[2] + checkResult[3]
-                + checkResult[4] + checkResult[5] + checkResult[6];
-        return result % 1000000007;
+        return (int) ((a0l0 + a0l1 + a0l2 + a1l0 + a1l1 + a1l2) % 1000000007);
     }
 
     /**
@@ -70,29 +59,14 @@ public class LeetCode552CheckRecord2 {
      * @return
      */
     public int checkRecord2(int n) {
-        if (n < 1)
-            return 0;
-        int[] checkResult = {1, 0, 0, 0, 1, 1, 0};
-        int[] checkResultCur;
-        for (int i = 2; i <= n; ++i) {
-            checkResultCur = new int[7];
-            checkResultCur[0] = checkResult[4] + checkResult[5] + checkResult[6];
-            checkResultCur[1] = checkResult[0] + checkResult[1] + checkResult[2] + checkResult[3];
-            checkResultCur[2] = checkResult[0] + checkResult[1];
-            checkResultCur[3] = checkResult[2];
-            checkResultCur[4] = checkResult[4] + checkResult[5] + checkResult[6];
-            checkResultCur[5] = checkResult[4];
-            checkResultCur[6] = checkResult[5];
-            checkResult[0] = checkResultCur[0];
-            checkResult[1] = checkResultCur[1];
-            checkResult[2] = checkResultCur[2];
-            checkResult[3] = checkResultCur[3];
-            checkResult[4] = checkResultCur[4];
-            checkResult[5] = checkResultCur[5];
-            checkResult[6] = checkResultCur[6];
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 3;
+        dp[2] = 8;
+        dp[3] = 19;
+        for (int i = 4; i <= n; ++i) {
+
         }
-        int result = checkResult[0] + checkResult[1] + checkResult[2] + checkResult[3]
-                + checkResult[4] + checkResult[5] + checkResult[6];
-        return result % 1000000007;
+        return dp[n];
     }
 }

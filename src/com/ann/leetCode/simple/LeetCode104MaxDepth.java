b326fef06c4de89baa8b1a8f7dd3b0d61ac264c6
chendan5 <chendan5@lenovo.com>
2020-04-16 00:11:22 +0800

    104

--- src/com/ann/leetCode/simple/LeetCode104MaxDepth.java
+++ src/com/ann/leetCode/simple/LeetCode104MaxDepth.java
@@ -1,4 +1,49 @@
 package com.ann.leetCode.simple;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LeetCode104MaxDepth {
+
+    // 广度优先
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int size = queue.size(), depth = 0;
+        while (size-- > 0) {
+            TreeNode node = queue.poll();
+            if (node.left != null)
+                queue.offer(node.left);
+            if (node.right != null)
+                queue.offer(node.right);
+            if (size == 0) {
+                size = queue.size();
+                ++depth;
+            }
+        }
+        return depth;
+    }
+
+    // todo ann DFS 迭代
+    public int maxDepth1(TreeNode root) {
+        return 0;
+    }
+
+    //递归 DFS
+    public int maxDepth2(TreeNode root) {
+        return 0;
+    }
+
+
+    class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 }

cf553e1f1b19b68875c46255214e887656f2e788
chendan5 <chendan5@lenovo.com>
2020-04-21 01:03:36 +0800

    annotation

--- src/com/ann/leetCode/simple/LeetCode160IntersectionNode.java
+++ src/com/ann/leetCode/simple/LeetCode160IntersectionNode.java
@@ -1,6 +1,8 @@
 package com.ann.leetCode.simple;
 
 public class LeetCode160IntersectionNode {
+
+    // 消除长度差     Len(list1) + len(List2) = len(list2) + len(list(1))
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode p1 = headA, p2 = headB;
         while (p1 != p2) {
